382e8f6932130ac9cea25ff6c0e17bc0ff54da8d
Elijah Silang <iv.elijah.s@gmail.com>
2021-01-06 20:02:48 -0600

    Drawer/Stack/Bottom Tab Navigation code included, interchangeable for testing purposes

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
 
-export default function App() {
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import LandingScreen from './screens/LandingScreen';
+
+import 'react-native-gesture-handler';
+
+const Drawer = createDrawerNavigator();
+
+const Stack = createStackNavigator();
+
+const BottomTab = createBottomTabNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomTab.Navigator initialRouteName="Home">
+        <BottomTab.Screen name="Home" component={HomeScreen} />
+        <BottomTab.Screen name="Login" component={LoginScreen} />
+        <BottomTab.Screen name="Landing" component={LandingScreen} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
